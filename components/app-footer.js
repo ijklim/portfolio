22fde3205386ddb4d28d03b0b754f6cbba2192da
Ivan Lim <aimprove@gmail.com>
2018-03-14 10:56:16 -0400

    Add footer

---
+++ components/app-footer.js
@@ -0,0 +1,31 @@
+Vue.component('app-footer', {
+  template: `
+    <v-footer dark class="px-3">
+      Proudly brought to you by
+      &nbsp;<a href="https://aiwebstudio.com" target="_blank">Ivan Lim</a>&nbsp; |
+      678-439-8880 |
+      &nbsp;<a href="https://aiwebstudio.com/contact-us/" target="_blank">Contact Us</a>&nbsp;
+    </v-footer>
+  `,
+  data () {
+    return {
+      
+    }
+  },
+  props: {
+    folderImage: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    
+  },
+  methods: {
+    
+  },
+  mounted () {
+    
+  }
+});
+
